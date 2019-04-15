64a6b59c970a7db7202d397e6fcec14f91deedf9
Ryan Mayberry <ryan@kerkness.ca>
2019-04-15 16:13:36 -0500

    features

---
+++ src/Schema/Feature.ts
@@ -0,0 +1,16 @@
+
+export enum Feature {
+    Artist = 1,
+    Gallery = 2,
+    Collector = 3,
+    Embedded = 4,
+    StandardPortfolio = 5,
+    EmergingPortfolio = 6,
+    EstablishedPortfolio = 7,
+    Editions = 8,
+    Shopify = 9,
+    ProStorage = 10,
+    AdvancedEditions = 11,
+    Watermark = 12,
+    LargeImports = 13,
+}
